7b2d30a2c5506e4cab6f85f6406d23207129bba5
Kollaider <kolja87@tut.by>
2022-09-11 22:31:42 +0300

    added check button

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+
 import config
 import telebot
 
@@ -6,13 +8,25 @@ from handle_data import handel_vacancies
 
 bot = telebot.TeleBot(config.token)
 
+@bot.message_handler(commands=['start'])
+def strat_bot(message):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = KeyboardButton("Проверить")
+    markup.add(item1)
+    bot.send_message(message.chat.id, 'Бот готов к поиску вакансий', reply_markup=markup)
 
-@bot.message_handler(commands=['check'])
+
+@bot.message_handler(regexp='Проверить')
 def get_vacancy(message):
     raw_data = parse_data()
     data = handel_vacancies(raw_data)
     for text in data[:5]:
         bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler()
+def another_answer(message):
+    bot.send_message(message.chat.id, 'Не понимаю.. Нажмите кнопку Проверить!')
 
 
 if __name__ == '__main__':
